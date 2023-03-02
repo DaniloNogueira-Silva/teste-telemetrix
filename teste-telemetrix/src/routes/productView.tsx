d148e89ae8178dbe221d65e4372d03062eef3bc1
DaniloNogueira-Silva <dansilva730x@gmail.com>
2023-03-02 07:53:17 -0300

    feat: Criar página de produtos

---
+++ teste-telemetrix/src/routes/productView.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/cards/card"
+import Sidebar from "../components/sidebar/sidebar"
+
+function ProductView() {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default ProductView
